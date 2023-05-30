897fc0c47fcfcfb2083fe2176775c88bb637578e
Alisinaakhlaqi <127827611+Alisinaakhlaqi@users.noreply.github.com>
2023-05-29 22:15:11 -0700

    Add files via upload

---
+++ Operator/LogicOpertor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOpertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            Console.WriteLine("Enter your Age");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" You Are " + ((Age > 30) && (Age > 20)) + " Young");
+            Console.WriteLine(" You Are " + ((Age < 30) && (Age < 50 )) + " Midle Man");
+            Console.WriteLine(" You Are " + ((Age == 30) || (Age<=20) || (Age>=50)) + " Comment");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Operator/NameSpace/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NameSpace2;
+using static System.Console;
+
+namespace NameSpace1
+{
+    class C1
+    {
+        public void F1()
+        {
+            WriteLine("You Are in \" Namespace 1\" and class \"C1\" And Function \"F1\"");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C1 obj = new C1();
+            NameSpace2.C1 obj2 = new NameSpace2.C1();
+            obj.F1();
+            obj2.F1();
+            Console.ReadLine();
+            //string ans;
+            //Console.WriteLine("Enter your ans");
+            //ans = Console.ReadLine();
+            //A.print(ans);
+            //Console.ReadLine();
+        }
+    }
+}
+namespace NameSpace2
+{
+    //class A
+    //{
+    //    static public void print(string x)
+    //    {
+    //        Console.WriteLine(x);
+    //    }
+    //}
+    class C1
+    {
+        public void F1()
+        {
+            Console.WriteLine("You Are in \" Namespace 2\" and class \"C1\" And Function \"F1\"");
+        }
+    }
+}
