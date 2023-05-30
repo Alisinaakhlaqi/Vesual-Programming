55d8f80599b5588d2e95c53acf7ef6104a6009d5
Alisinaakhlaqi <127827611+Alisinaakhlaqi@users.noreply.github.com>
2023-05-29 22:12:27 -0700

    Add files via upload

---
+++ debuging/debuging/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace debuging
+{
+    class Program
+    {
+        static int mod (int a,int b)=>(a*b);
+
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("program is in main");
+            Console.Write("Enter the first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Debug.WriteLine($"Entred{a} & {b} ");
+            Console.WriteLine($"result { mod(a, b)}");
+            Debug.WriteLine("main method Finished");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ debuging/second/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second
+{
+    class Program
+    {
+        static int Get_number()
+        {
+            Console.WriteLine("enter int number = ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int a = Get_number();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("program finished");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
