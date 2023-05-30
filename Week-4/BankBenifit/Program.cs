738fa6708a38b0591846784b1d514b3a2ab399f3
Alisinaakhlaqi <127827611+Alisinaakhlaqi@users.noreply.github.com>
2023-05-29 22:18:05 -0700

    Add files via upload

---
+++ Week-4/BankBenifit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBenifit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Blance, BlanceGold, BankBenifit;
+            int mounthWait;
+            Console.WriteLine("Enter Blance :");
+            Blance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Gold :");
+            BlanceGold = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Bank Benifit :");
+            BankBenifit = Convert.ToDouble(Console.ReadLine());
+            mounthWait = 0;
+            while (Blance <= BlanceGold)
+            {
+                Blance = (Blance * (BankBenifit / 100)) + Blance;
+                mounthWait++;
+                Console.WriteLine("Your Blance After: \t" + mounthWait + "\t is \t" + Blance);
+
+            }
+          
+
+            Console.WriteLine("Number of mounth they you should waite is \t" + mounthWait);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-4/Branching/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B;
+            char op;
+               
+            Console.Write("Inter First Number : ");
+            A = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Inter First Number : ");
+            B = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Inter First Opertor : ");
+            op = Convert.ToChar(Console.ReadLine());
+            //if (op == '+')
+            //    Console.WriteLine(A + B);
+            //else if (op == '-')
+            //    Console.WriteLine(A - B);
+            //else if (op == '*')
+            //    Console.WriteLine(A * B);
+            //else if (op == '/')
+            //    Console.WriteLine(A / B);
+            //else
+            //    Console.WriteLine("ERROR");
+            
+            switch(op){
+                case '+':
+                    {
+                        Console.WriteLine(A + B);
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine(A - B);
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine(A * B);
+                        break;
+                    }
+                case '/':
+                    {
+                        if()
+                        Console.WriteLine(A / B);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("ERROR");
+                        break;
+                    }
+            }
+            Console.ReadLine();
+        }
+    }
+}
