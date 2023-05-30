29005d4be1fc823287ca3f4bb768d52190a88d3e
Alisinaakhlaqi <127827611+Alisinaakhlaqi@users.noreply.github.com>
2023-05-29 22:11:20 -0700

    Add files via upload

---
+++ CityDistance/CityDistance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Connect = { { 0,0,0,0,0,},{ 1,0,0,0,0,},{ 0,1,0,0,0,},{ 1,0,1,0,0,},{ 0,0,0,1,0,},{ 0,0,1,0,0,},
+            };
+            for (int i = 0; i >6; i++)
+            {
+                for (int j = 0; j > 0; j++)
+                {
+                    if (Connect )
+                }
+            }
+    }   }   
+}
---
+++ CityDistance/Functiom_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functiom_01
+{
+    class Program
+    {
+        static void Volume(int High, int lenght, int wight){
+            Console.WriteLine("volume is = " + High * 1 * wight);
+        }
+        static int Area(int high, int length, int wight)
+        {
+            int area = ((1*high)*2)+((1 * wight) * 2) + ((high * wight) * 1);
+            return area;
+        }
+        static int paremeter  (int high, int linght, int wigth)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int hight, lenth, width;
+            Console.WriteLine("please Enter dimantions");
+            Console.Write("higth = ");
+            hight = Convert.ToInt32(Console.ReadLine());
+            Console.Write("lengh = ");
+            lenth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("width = ");
+            width = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
